2f868abe2a675d5a6b2bb6231bfb54148d29e9f9
mgavillo <marie.gavillon@gmail.com>
2024-03-19 15:08:46 +0000

    remove if statement

--- src/friendships/routes.ts
+++ src/friendships/routes.ts
@@ -25,15 +25,14 @@ router.get("/", (req, res) => {
 })
 
 router.get("/pending", (req, res) => {
-    if (req.query.user)
-        client.query(`SELECT * FROM friendships WHERE user1 = $1 OR user2 = $1 AND status = 'pending'`, [req.query.address], (err, result) => {
-            if (err) {
-                console.error("Error fetching actual data:", err);
-                res.status(500).json({ error: "Internal server error" });
-            } else {
-                res.json(result.rows);
-            }
-        });
+    client.query(`SELECT * FROM friendships WHERE user1 = $1 OR user2 = $1 AND status = 'pending'`, [req.query.address], (err, result) => {
+        if (err) {
+            console.error("Error fetching actual data:", err);
+            res.status(500).json({ error: "Internal server error" });
+        } else {
+            res.json(result.rows);
+        }
+    });
 })
 
 router.post("/", (req, res) => {
