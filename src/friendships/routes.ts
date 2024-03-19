450cc668bc847b3c39a398ca837eb8ac4fea5b96
mgavillo <marie.gavillon@gmail.com>
2024-03-19 14:16:47 +0000

    add get all friendships

--- src/friendships/routes.ts
+++ src/friendships/routes.ts
@@ -4,14 +4,24 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    client.query(`SELECT * FROM friendships user1 = $1 OR user2 =$1`, [req.query.user], (err, result) => {
-        if (err) {
-            console.error("Error fetching actual data:", err);
-            res.status(500).json({ error: "Internal server error" });
-        } else {
-            res.json(result.rows);
-        }
-    });
+    if (req.query.user)
+        client.query(`SELECT * FROM friendships user1 = $1 OR user2 =$1`, [req.query.user], (err, result) => {
+            if (err) {
+                console.error("Error fetching actual data:", err);
+                res.status(500).json({ error: "Internal server error" });
+            } else {
+                res.json(result.rows);
+            }
+        });
+    else
+        client.query(`SELECT * FROM friendships`, (err, result) => {
+            if (err) {
+                console.error("Error fetching actual data:", err);
+                res.status(500).json({ error: "Internal server error" });
+            } else {
+                res.json(result.rows);
+            }
+        });
 })
 
 router.post("/", (req, res) => {
