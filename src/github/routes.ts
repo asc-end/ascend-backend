47382456f879ccb57c2b9b277212011e33ffb4fb
mgavillo <marie.gavillon@gmail.com>
2024-03-30 18:36:52 +0000

    revoke working

--- src/github/routes.ts
+++ src/github/routes.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { Octokit } from "octokit";
-
+import { createOAuthUserAuth } from "@octokit/auth-oauth-user";
 const router = express.Router();
 
 router.get("/repo", (req, res) => {
@@ -23,20 +23,16 @@ router.get("/repo", (req, res) => {
 })
 
 router.get("/revoke", (req, res) => {
-    const { token } = req.query
+    const { token } = req.body
     const octokit = new Octokit({
-        auth: token,
+        authStrategy: createOAuthUserAuth,
+        auth: {clientId: process.env.GH_CLIENT_ID, clientSecret: process.env.GH_CLIENT_SECRET}
     });
 
-    octokit.rest.apps.deleteAuthorization({
-        client_id: "d42d5aa2eacfe82135e7",
-        access_token: token as string,
-    }).then((result) => {
+    octokit.request(`DELETE /applications/${process.env.GH_CLIENT_ID}/grant`, { access_token: token }).then((result) => {
         console.log(result)
         res.status(200).json("Authorization revoked properly")
     }).catch((e) => res.status(500).json(`An error occured while revoking: ${e}`))
-
-
 })
 
 export default router 
