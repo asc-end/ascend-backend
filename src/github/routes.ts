af37f59ef3b149344d168cfe2e86b943bcefb88e
mgavillo <marie.gavillon@gmail.com>
2024-03-30 18:13:24 +0000

    try revoke

--- src/github/routes.ts
+++ src/github/routes.ts
@@ -22,4 +22,21 @@ router.get("/repo", (req, res) => {
     }).catch(e => res.status(500).json({ error: e }))
 })
 
+router.post("/revoke", (req, res) => {
+    const { token } = req.query
+    const octokit = new Octokit({
+        auth: token,
+    });
+
+    octokit.rest.apps.deleteAuthorization({
+        client_id: "d42d5aa2eacfe82135e7",
+        access_token: token as string,
+    }).then((result) => {
+        console.log(result)
+        res.status(200).json("Authorization revoked properly")
+    }).catch((e) => res.status(500).json(`An error occured while revoking: ${e}`))
+
+
+})
+
 export default router 
