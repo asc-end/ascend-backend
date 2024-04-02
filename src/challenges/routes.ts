bba6bfb30f9103f55f9244f04fa768ea4183f0d1
mgavillo <marie.gavillon@gmail.com>
2024-04-02 20:43:29 +0100

    add cursor

--- src/challenges/routes.ts
+++ src/challenges/routes.ts
@@ -4,7 +4,7 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const {address} = req.query
+    const { address } = req.query
 
     client.query(`SELECT * FROM challenges WHERE players::text LIKE '%' || $1 || '%' OR author = $1`, [address], (err, result) => {
         if (err) {
@@ -18,30 +18,40 @@ router.get("/", (req, res) => {
 
 router.get("/feed", async (req, res) => {
     try {
-      const challengesQuery = await client.query(`SELECT * FROM challenges ORDER BY beginDate`);
-      const challenges = challengesQuery.rows;
-  
-      const usersPromises = challenges.map(async (challenge) => {
-        const userQuery = await client.query(`SELECT * FROM users WHERE address = $1`, [challenge.author]);
-        return userQuery.rows[0];
-      });
-  
-      const users = await Promise.all(usersPromises);
-  
-      // Replace author with user object
-      const challengesWithUsers = challenges.map((challenge, index) => {
-        return {
-          ...challenge,
-          author: users[index]
-        };
-      });
-  
-      res.json(challengesWithUsers);
+        const cursorParam = req.query.cursor; // Get cursor from query parameters
+        const cursor = typeof cursorParam === 'string' ? parseInt(cursorParam) : 0; // Convert cursor to number, default to 0 if not provided or invalid
+        const limit = 10; // Set a limit for the number of challenges per page
+
+        const challengesQuery = await client.query(`SELECT * FROM challenges ORDER BY beginDate OFFSET $1 LIMIT $2`, [cursor, limit]);
+        const challenges = challengesQuery.rows;
+
+        const usersPromises = challenges.map(async (challenge) => {
+            const userQuery = await client.query(`SELECT * FROM users WHERE address = $1`, [challenge.author]);
+            return userQuery.rows[0];
+        });
+
+        const users = await Promise.all(usersPromises);
+
+        // Replace author with user object
+        const challengesWithUsers = challenges.map((challenge, index) => {
+            return {
+                ...challenge,
+                author: users[index]
+            };
+        });
+
+        let nextCursor = null;
+        if (challenges.length >= limit) {
+            // If there are more challenges, set the next cursor as the next sequential number after the last challenge retrieved
+            nextCursor = cursor + challenges.length;
+        }
+
+        res.json({ challenges: challengesWithUsers, nextCursor });
     } catch (err) {
-      console.error("Error fetching actual data:", err);
-      res.status(500).json({ error: "Internal server error" });
+        console.error("Error fetching actual data:", err);
+        res.status(500).json({ error: "Internal server error" });
     }
-  });
+});
 
 router.post("/new", (req, res) => {
     try {
