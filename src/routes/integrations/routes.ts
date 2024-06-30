4b9168ee88ec06e82cc8e862658b78cfa7b5c543
mgavillo <marie.gavillon@gmail.com>
2024-06-30 21:30:53 +0400

    remove user integration

--- src/routes/integrations/routes.ts
+++ src/routes/integrations/routes.ts
@@ -26,4 +26,27 @@ router.get("/user", async (req, res) => {
     }
 })
 
+router.delete("/user", async (req, res) => {
+    try {
+        const { address, app } = req.query
+        if (!address) return res.status(400).json({ error: "Please provide an address." })
+
+        // Change the query to delete the user with the provided address
+        const query = `DELETE FROM app_profiles WHERE address = $1 AND app = $2`;
+
+        client.query(query, [address, app], async (err, result) => {
+            if (err) throw Error("Failed to delete profile")
+
+            // If no rows were affected, the user was not found
+            if (result.rowCount == 0)
+                return res.status(404).json({ error: "User not found" })
+
+            res.status(200).json({ message: "User deleted successfully" });
+        })
+    } catch (err) {
+        console.error('Error deleting user:', err);
+        res.status(500).json({ error: `Internal server error ${err}` });
+    }  
+})
+
 export default router 
