fad707fc01b4dbeaf48e4a66420391b4a8bce1d5
mgavillo <marie.gavillon@gmail.com>
2024-06-30 21:22:04 +0400

    update route for integration profile

--- src/routes/integrations/routes.ts
+++ src/routes/integrations/routes.ts
@@ -6,18 +6,16 @@ const router = express.Router();
 
 router.get("/user", async (req, res) => {
     try {
-        const {address} = req.query
+        const { address } = req.query
         if (!address) return res.status(400).json({ error: "Please provide an address." })
 
-        const query = `SELECT * FROM app_profiles WHERE user_address = $1`;
+        const query = `SELECT * FROM app_profiles WHERE address = $1`;
 
-        client.query(query, [address,], async (err, result) => {
-            if (err) {
-                throw Error("no linked farcaster")
-            }
+        client.query(query, [address], async (err, result) => {
+            if (err) throw Error("no linked profile")
 
-            if(result.rows.length == 0)
-                return res.status(200).json({users: []})
+            if (result.rows.length == 0)
+                return res.status(200).json({ users: [] })
 
             // const users:User[] = (await neynarClient.fetchBulkUsers(result.rows.map(r => r.fid))).users
             res.status(200).json({ users: result.rows });
