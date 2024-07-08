c06d9ba7b0c49dfada3c9bf1a822c9431d00c0be
mgavillo <marie.gavillon@gmail.com>
2024-07-09 02:21:22 +0400

    add farcaster webhook

--- src/routes/integrations/farcaster/routes.ts
+++ src/routes/integrations/farcaster/routes.ts
@@ -4,6 +4,7 @@ import client from "../../../lib/db";
 import { createAppClient, viemConnector } from "@farcaster/auth-client";
 import neynarClient from "../../../lib/neynar";
 import { User } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import { validate } from "../../../lib/solana/validate";
 const router = express.Router();
 
 export const appClient = createAppClient({
@@ -63,8 +64,8 @@ router.get("/user", async (req, res) => {
 
         client.query(query, [address,], async (err, result) => {
             if (err) throw Error("no linked farcaster")
-            if(result.rows.length == 0) return res.status(200).json({users: []})
-            const users:User[] = (await neynarClient.fetchBulkUsers(result.rows.map(r => r.app_id))).users
+            if (result.rows.length == 0) return res.status(200).json({ users: [] })
+            const users: User[] = (await neynarClient.fetchBulkUsers(result.rows.map(r => r.app_id))).users
             res.status(200).json({ users: users });
         })
     } catch (err) {
@@ -91,6 +92,72 @@ router.delete("/user", async (req, res) => {
     } catch (err) {
         console.error('Error creating user:', err);
         res.status(500).json({ error: `Internal server error ${err}` });
+    }
+})
+
+router.post("/webhook", async (req, res) => {
+    try {
+        const { fid, challengeId } = req.body
+
+        const webhookUrl = "https://ascend-backend-production.up.railway.app/integrations/farcaster/webhook/cast"
+
+        const url = 'https://api.neynar.com/v2/farcaster/webhook';
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                api_key: process.env.NEYNAR_API_KEY!!,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                subscription: { 'cast.created': { author_fids: [fid] } },
+                name: `Ascend-Challenge-${challengeId}`,
+                url: webhookUrl
+            })
+        };
+
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
+                res.status(200).json("Webhook created successfully")
+            })
+    } catch (e) {
+        res.status(500).json({ error: e })
+    }
+})
+
+router.post("/webhook/cast", (req, res) => {
+    try {
+
+        console.log("WARPCAST WEBOOK POST")
+
+        // Extract necessary data from the request
+        const fid = req.body.pusher.name;
+
+        const challengeQuery = `
+        SELECT cp.*, u.address AS user_address, c.author AS author_address, c.solanaid
+            FROM challenges_players cp
+            JOIN users u ON cp.address = u.address
+            JOIN challenges c ON cp.main_id = c.id
+            WHERE cp.target = $1 AND cp.user_name = $2
+        `;
+
+        client.query(challengeQuery, ["Farcaster", fid], async (err, challengeResult) => {
+            if (err)
+                return res.status(500).json({ error: `Internal server error : ${err.message}` });
+            if (challengeResult.rows.length === 0)
+                return res.status(404).json({ message: "No pending challenge found" });
+
+            const currentChallenge = challengeResult.rows[0];
+            let resp = await validate(currentChallenge.solanaid, currentChallenge.author_address, currentChallenge.user_address)
+
+            if (!resp) throw Error()
+            res.status(200).json({ message: "Successfully validated day." })
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ error: e })
     }
 })
 
